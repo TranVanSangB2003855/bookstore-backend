5a5ef47e81d2cce2182ce2d78e008d029a31d69b
TranVanSangB2003855 <96764199+TranVanSangB2003855@users.noreply.github.com>
2023-01-03 21:45:37 +0700

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,9 @@
 const config = {
     app: {
-        port: process.env.port || 3000 ,
+        port:3000 ,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb+srv://admin:TRVASAmb6902@cluster0.ekgkgef.mongodb.net"
+        uri: "mongodb+srv://admin:TRVASAmb6902@cluster0.ekgkgef.mongodb.net"
         //"mongodb://127.0.0.1:27017/bookstore"
     },
     secret: "bezkoder-secret-key"
